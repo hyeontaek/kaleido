8f0ef0913ee542ed2a4702d68d8ab38f8e16d6f7
Hyeontaek Lim <hl@cs.cmu.edu>
2011-08-23 20:21:37 -0400

    added git path specification

--- git-sync.py
+++ git-sync.py
@@ -31,12 +31,12 @@ META_DEFAULT='.sync'
 #META_DEFAULT='.git'    # for debugging; unsafe
 
 if platform.platform().startswith('Linux') or platform.platform().startswith('FreeBSD'):
-    GIT_PATH = 'git'
+    GIT_DEFAULT = 'git'
 elif platform.platform().startswith('Windows'):
     if platform.architecture()[0] == '64bit':
-        GIT_PATH = r'C:\Program Files (x86)\Git\bin\git.exe'
+        GIT_DEFAULT= r'C:\Program Files (x86)\Git\bin\git.exe'
     else:
-        GIT_PATH = r'C:\Program Files\Git\bin\git.exe'
+        GIT_DEFAULT = r'C:\Program Files\Git\bin\git.exe'
 else:
     assert False
 
@@ -48,8 +48,8 @@ def move_output(src, dst, tee=None):
         if tee: tee.write(s)
     src.close()
 
-def run_git(args, print_stdout=True, print_stderr=True, fatal=False):
-    p = subprocess.Popen([GIT_PATH] + args, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=os.environ)
+def run(git_path, args, print_stdout=True, print_stderr=True, modify_path=True, fatal=False):
+    p = subprocess.Popen([git_path] + args, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     p.stdin.close()
 
@@ -69,24 +69,24 @@ def run_git(args, print_stdout=True, print_stderr=True, fatal=False):
     
     return (ret == 0, stdout_buf.getvalue(), stderr_buf.getvalue())
 
-def detect_git_version():
-    _, version, _ = run_git(['--version'], False, False, fatal=True)
+def detect_git_version(git_path):
+    _, version, _ = run(git_path, ['--version'], False, False, fatal=True)
 
     version = version.split(' ')[2]
     version = tuple([int(x) for x in version.split('.')[:4]])
     return version
 
-def list_git_branches():
-    for line in run_git(['branch', '--no-color'], False, False)[1].splitlines():
+def list_git_branches(git_path, path_args):
+    for line in run(git_path, path_args + ['branch', '--no-color'], False, False)[1].splitlines():
         yield line[2:].strip()
 
-def update_env(directory, meta):
-    os.environ['GIT_DIR'] = os.path.join(directory, meta)
-    os.environ['GIT_WORK_TREE'] = directory
+def get_path_args(directory, meta):
+    return ['--git-dir=' + os.path.join(directory, meta), '--work-tree=' + directory]
 
 def main():
-    usage = 'usage: %prog [--meta <directory>] {--custom <git-command> | [{--init | --clone <repository>}] [--serve [<address>:]<port>] [--no-sync | --sync-forever] <directory>}'
+    usage = 'usage: %prog [--meta <directory>] {--usercmd <git-command> | [{--init | --clone <repository>}] [--serve [<address>:]<port>] [--no-sync | --sync-forever] <directory>}'
     parser = optparse.OptionParser(usage=usage)
+    parser.add_option('-g', '--git', dest='git', default=GIT_DEFAULT)
     parser.add_option('-m', '--meta', dest='meta', default=META_DEFAULT)
     parser.add_option('-U', '--usercmd', dest='usercmd', action='store_true', default=False)
     parser.add_option('-i', '--init', dest='init', action='store_true', default=False)
@@ -97,12 +97,12 @@ def main():
     (options, args) = parser.parse_args()
 
     if options.usercmd:
-        args = []
+        args = get_path_args('.', options.meta)
         for idx, arg in enumerate(sys.argv):
             if arg == '-U' or arg == '--custom':
-                args = sys.argv[idx + 1:]
+                args += sys.argv[idx + 1:]
                 break
-        return 0 if run_git(args)[0] else 1
+        return 0 if run(options.git, args)[0] else 1
 
     if len(args) != 1:
         parser.error('directory must be given')
@@ -120,19 +120,19 @@ def main():
         return 1
 
     directory = args[0]
+    path_args = get_path_args(directory, options.meta)
 
     if options.init:
         if os.path.exists(os.path.join(directory, options.meta)):
             print('warning: meta directory already exists; ignoring --init')
         else:
-            run_git(['init', '--bare', os.path.join(directory, options.meta)], fatal=True)
-            update_env(directory, options.meta)
-            run_git(['config', 'core.bare', 'false'], fatal=True)
-            run_git(['commit', '--author="sync <sync@sync>"', '--message=""', '--allow-empty'], True, fatal=True)
+            run(options.git, ['init', '--bare', os.path.join(directory, options.meta)], fatal=True)
+            run(options.git, path_args + ['config', 'core.bare', 'false'], fatal=True)
+            run(options.git, path_args + ['commit', '--author="sync <sync@sync>"', '--message=""', '--allow-empty'], True, fatal=True)
             open(os.path.join(directory, options.meta, 'info', 'exclude'), 'at').write(options.meta + '\n')
-            open(os.path.join(directory, options.meta, 'git-daemon-export-ok'), 'w')
+            open(os.path.join(directory, options.meta, 'git-daemon-export-ok'), 'wb')
             inbox_id = '%d_%d' % (time.time(), random.randint(0, 999999))
-            open(os.path.join(directory, options.meta, 'inbox-id'), 'w').write(inbox_id)
+            open(os.path.join(directory, options.meta, 'inbox-id'), 'wb').write(inbox_id)
             return 0
 
     elif options.clone != None:
@@ -140,23 +140,20 @@ def main():
             print('warning: meta directory already exists; ignoring --clone')
         else:
             url = options.clone.rstrip('/') + '/' + options.meta
-            update_env(directory, options.meta)
-            run_git(['clone', '--bare', url, os.path.join(directory, options.meta)], fatal=True)
-            run_git(['config', 'core.bare', 'false'], fatal=True)
-            run_git(['config', 'remote.origin.url', url], fatal=True)
+            run(options.git, path_args + ['clone', '--bare', url, os.path.join(directory, options.meta)], fatal=True)
+            run(options.git, path_args + ['config', 'core.bare', 'false'], fatal=True)
+            run(options.git, path_args + ['config', 'remote.origin.url', url], fatal=True)
             open(os.path.join(directory, options.meta, 'info', 'exclude'), 'at').write(options.meta + '\n')
-            open(os.path.join(directory, options.meta, 'git-daemon-export-ok'), 'w')
+            open(os.path.join(directory, options.meta, 'git-daemon-export-ok'), 'wb')
             inbox_id = '%d_%d' % (time.time(), random.randint(0, 999999))
-            open(os.path.join(directory, options.meta, 'inbox-id'), 'w').write(inbox_id)
-            run_git(['checkout'], fatal=True)
+            open(os.path.join(directory, options.meta, 'inbox-id'), 'wb').write(inbox_id)
+            run(options.git, path_args + ['checkout'], fatal=True)
             return 0
 
     if not os.path.exists(os.path.join(directory, options.meta)):
         print('error: meta directory does not exist')
         return 1
-    inbox_id = open(os.path.join(directory, options.meta, 'inbox-id'), 'r').read()
-
-    update_env(directory, options.meta)
+    inbox_id = open(os.path.join(directory, options.meta, 'inbox-id'), 'rb').read()
 
     t_serve = None
     if options.serve != None:
@@ -166,8 +163,8 @@ def main():
             address = '0.0.0.0'
             port = options.serve
         t_serve = threading.Thread(
-                target=run_git,
-                args=(['daemon', '--strict-paths', '--reuseaddr',
+                target=run,
+                args=(options.git, path_args + ['daemon', '--strict-paths', '--reuseaddr',
                         '--enable=upload-pack', '--enable=upload-archive', '--enable=receive-pack',
                         '--listen=' + address, '--port=' + port,
                         '--base-path=' + os.path.abspath(directory),
@@ -175,33 +172,33 @@ def main():
             )
         t_serve.start()
 
-    version = detect_git_version()
+    version = detect_git_version(options.git)
     if version >= (1, 7, 0, 0):
         git_strategy_option = ['-X', 'theirs']
     else:
         git_strategy_option = []
     
-    if run_git(['config', '--get', 'remote.origin.url'], False)[0]:
+    if run(options.git, ['config', '--get', 'remote.origin.url'], False)[0]:
         git_pushable = True
     else:
         git_pushable = False
 
     while options.sync_forever:
         # merge local sync_inbox_* into local master
-        for branch in list_git_branches():
+        for branch in list_git_branches(options.git, path_args):
             if not branch.startswith('sync_inbox_'):
                 continue
-            run_git(['merge', '--quiet', '--strategy=recursive'] + git_strategy_option + [branch])
+            run(options.git, path_args + ['merge', '--quiet', '--strategy=recursive'] + git_strategy_option + [branch])
 
         # commit local changes to local master
-        run_git(['add', '--all'])
-        run_git(['commit', '--author="sync <sync@sync>"', '--message=""'], False)
+        run(options.git, path_args + ['add', '--all'])
+        run(options.git, path_args + ['commit', '--author="sync <sync@sync>"', '--message=""'], False)
 
         if git_pushable:
             # push local master to remote sync_inbox_ID for remote merge
-            run_git(['push', '--quiet', 'origin', 'master:sync_inbox_%d' % inbox_id], False)
+            run(options.git, path_args + ['push', '--quiet', 'origin', 'master:sync_inbox_%d' % inbox_id], False)
             # fetch remote master to local sync_inbox_origin for local merge
-            run_git(['fetch', '--quiet', 'origin', 'master:sync_inbox_origin'])
+            run(options.git, path_args + ['fetch', '--quiet', 'origin', 'master:sync_inbox_origin'])
 
         time.sleep(1)
 
