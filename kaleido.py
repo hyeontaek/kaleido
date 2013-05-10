ca2da27fca4fa836a74ae21dbabee47b8d04fdb8
Hyeontaek Lim <hl@cs.cmu.edu>
2013-05-10 19:41:33 -0400

    code and output cleanups

--- kaleido.py
+++ kaleido.py
@@ -1,6 +1,4 @@
-#!/usr/bin/env python
-
-# git-sync: a file synchronizer using git as transport
+# kaleido: a multi-way file synchronizer using git as transport
 # Copyright (C) 2011,2013 Hyeontaek Lim
 #
 # This program is free software; you can redistribute it and/or
@@ -17,7 +15,6 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-import BaseHTTPServer
 import optparse
 import os
 import platform
@@ -30,17 +27,18 @@ import threading
 
 META_DEFAULT='.sync'
 #META_DEFAULT='.git'    # for debugging; unsafe
+
 WORKING_COPY_DEFAULT='.'
 
 if platform.platform().startswith('Linux') or platform.platform().startswith('FreeBSD'):
-    GIT_DEFAULT = '/usr/bin/git'
+    GIT_DEFAULT = 'git'
 elif platform.platform().startswith('Windows'):
     if platform.architecture()[0] == '64bit':
         GIT_DEFAULT= r'C:\Program Files (x86)\Git\bin\git.exe'
     else:
         GIT_DEFAULT = r'C:\Program Files\Git\bin\git.exe'
 else:
-    assert False
+    assert False, 'not support platform'
 
 def copy_output(src, dst, tee=None):
     while True:
@@ -67,7 +65,7 @@ def run(git_path, args, print_stdout=True, print_stderr=True, fatal=False):
     list([t.join() for t in threads])
 
     if fatal and ret != 0:
-        raise RuntimeError('git returned %d' % ret)
+        raise RuntimeError('%s returned %d' % (git_path, ret))
     
     return (ret == 0, stdout_buf.getvalue(), stderr_buf.getvalue())
 
@@ -86,33 +84,27 @@ def get_path_args(directory, meta):
     return ['--git-dir=' + os.path.join(directory, meta), '--work-tree=' + directory]
 
 def init(options, command, args):
-    if os.path.exists(os.path.join(options.working_copy, options.meta)):
-        print('error: meta directory already exists')
-        return False
+    inbox_id = '%d_%d' % (time.time(), random.randint(0, 999999))
     git_common_options = get_path_args(options.working_copy, options.meta)
     run(options.git, ['init', '--bare', os.path.join(options.working_copy, options.meta)], fatal=True) # this does not use git_common_options
     run(options.git, git_common_options + ['config', 'core.bare', 'false'], fatal=True)
-    run(options.git, git_common_options + ['commit', '--author="sync <sync@sync>"', '--message=""', '--allow-empty'], fatal=True)
+    run(options.git, git_common_options + ['commit', '--author="%s <%s@kaleido>"' % (inbox_id, inbox_id), '--message=', '--allow-empty-message', '--allow-empty'], fatal=True)
     open(os.path.join(options.working_copy, options.meta, 'info', 'exclude'), 'at').write(options.meta + '\n')
     open(os.path.join(options.working_copy, options.meta, 'git-daemon-export-ok'), 'wb')
-    inbox_id = '%d_%d' % (time.time(), random.randint(0, 999999))
     open(os.path.join(options.working_copy, options.meta, 'inbox-id'), 'wb').write(inbox_id + '\n')
     return True
 
 def clone(options, command, args):
-    if os.path.exists(os.path.join(options.working_copy, options.meta)):
-        print('error: meta directory already exists')
-        return False
     url = args[0].rstrip('/') + '/' + options.meta
     if url.find('://') == -1:
         url = os.path.abspath(url)
+    inbox_id = '%d_%d' % (time.time(), random.randint(0, 999999))
     git_common_options = get_path_args(options.working_copy, options.meta)
     run(options.git, git_common_options + ['clone', '--bare', url, os.path.join(options.working_copy, options.meta)], fatal=True)
     run(options.git, git_common_options + ['config', 'core.bare', 'false'], fatal=True)
     run(options.git, git_common_options + ['config', 'remote.origin.url', url], fatal=True)
     open(os.path.join(options.working_copy, options.meta, 'info', 'exclude'), 'at').write(options.meta + '\n')
     open(os.path.join(options.working_copy, options.meta, 'git-daemon-export-ok'), 'wb')
-    inbox_id = '%d_%d' % (time.time(), random.randint(0, 999999))
     open(os.path.join(options.working_copy, options.meta, 'inbox-id'), 'wb').write(inbox_id + '\n')
     run(options.git, git_common_options + ['checkout'], fatal=True)
     return True
@@ -120,24 +112,16 @@ def clone(options, command, args):
 def serve(options, command, args):
     address_arg = args[0]
     address, port = address_arg.split(':', 1) if address_arg.find(':') != -1 else ('0.0.0.0', address_arg)
-    dirs = []
-    if not options.quiet:
-        print('served:')
-    for name in ['.'] + os.listdir(options.working_copy):
-        path = os.path.abspath(os.path.join(options.working_copy, name, options.meta))
-        if os.path.isdir(path):
-            dirs.append(path)
-            if not options.quiet:
-                print(name)
+    base_path, _, repo = os.path.abspath(options.working_copy).partition(os.sep)
     git_common_options = get_path_args(options.working_copy, options.meta)
     ret = run(options.git, git_common_options + ['daemon', '--reuseaddr', '--strict-paths',
             '--enable=upload-pack', '--enable=upload-archive', '--enable=receive-pack',
             '--listen=' + address, '--port=' + port,
-            '--base-path=' + os.path.abspath(options.working_copy)] + dirs)
+            '--base-path=' + base_path, repo])
     return ret[0]
 
 def sync(options, command, args):
-    sync_forever = command == 'sync-forever'
+    sync_forever = (command == 'sync-forever')
     inbox_id = open(os.path.join(options.working_copy, options.meta, 'inbox-id'), 'rb').read().strip()
 
     version = detect_git_version(options.git)
@@ -156,7 +140,9 @@ def sync(options, command, args):
 
             # commit local changes to local master
             run(options.git, git_common_options + ['add', '--update'], print_stdout=(not options.quiet))
-            run(options.git, git_common_options + ['commit', '--quiet', '--author="sync <sync@sync>"', '--message=""'], print_stdout=(not options.quiet))
+            if not run(options.git, git_common_options + ['diff', '--quiet', '--cached'])[0]:
+                # there seems some change to commit
+                run(options.git, git_common_options + ['commit', '--quiet', '--author="%s <%s@kaleido>"' % (inbox_id, inbox_id), '--message=', '--allow-empty-message'], print_stdout=(not options.quiet))
 
             if git_pushable:
                 # push local master to remote sync_inbox_ID for remote merge
@@ -173,20 +159,19 @@ def sync(options, command, args):
     return True
 
 def git(options, command, args):
-    git_args = get_path_args('.', options.meta) + args
+    git_args = get_path_args('.', options.meta) + [command] + args
     return run(options.git, git_args)[0]
 
 def main():
-    usage = 'usage: %prog [OPTIONS] {init | clone <repository> | serve [<address>:]<port> | sync | sync-forever | git [<git-command>]}'
+    usage = 'usage: %prog [OPTIONS] {init | clone <repository> | serve [<address>:]<port> | sync | sync-forever | <git-command>}'
     parser = optparse.OptionParser(usage=usage)
-    parser.add_option('-g', '--git', dest='git', default=GIT_DEFAULT, help='git executable path')
-    parser.add_option('-m', '--meta', dest='meta', default=META_DEFAULT, help='git repository directory name')
-    parser.add_option('-w', '--working-copy', dest='working_copy', default=WORKING_COPY_DEFAULT, help='working copy path')
-    parser.add_option('-q', '--quiet', dest='quiet', action='store_true', default=False, help='less verbose')
+    parser.add_option('-g', '--git', dest='git', default=GIT_DEFAULT, help='git executable path [default: %default]')
+    parser.add_option('-m', '--meta', dest='meta', default=META_DEFAULT, help='git repository directory name [default: %default]')
+    parser.add_option('-w', '--working-copy', dest='working_copy', default=WORKING_COPY_DEFAULT, help='working copy path [default: %default]')
+    parser.add_option('-q', '--quiet', dest='quiet', action='store_true', default=False, help='less verbose when syncing')
     (options, args) = parser.parse_args()
 
     if len(args) == 0:
-        parser.error('too few arguments')
         parser.print_help()
         return 1
 
@@ -194,11 +179,18 @@ def main():
     args = args[1:]
 
     if command == 'init':
-        ret = init(options, command, args)
+        if os.path.exists(os.path.join(options.working_copy, options.meta)):
+            print('error: %s directory already exists' % options.meta)
+            ret = False
+        else:
+            ret = init(options, command, args)
     elif command == 'clone':
         if len(args) < 1:
             parser.error('too few arguments')
             parser.print_help()
+            ret = False
+        elif os.path.exists(os.path.join(options.working_copy, options.meta)):
+            print('error: %s directory already exists' % options.meta)
             ret = False
         else:
             ret = clone(options, command, args)
@@ -211,16 +203,12 @@ def main():
             ret = serve(options, command, args)
     elif command == 'sync' or command == 'sync-forever':
         if not os.path.exists(os.path.join(options.working_copy, options.meta)):
-            print('error: meta directory does not exist')
+            print('error: %s directory does not exist' % options.meta)
             ret = False
         else:
             ret = sync(options, command, args)
-    elif command == 'git':
-        ret = git(options, command, args)
     else:
-        print('error: invalid command')
-        parser.print_help()
-        ret = False
+        ret = git(options, command, args)
 
     return 0 if ret else 1
 
