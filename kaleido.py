96dd29c4bac53e1dd23f850134d55f9a51147f47
Hyeontaek Lim <hl@cs.cmu.edu>
2013-06-20 03:24:10 -0400

    retrying one file per commit

--- kaleido.py
+++ kaleido.py
@@ -628,7 +628,7 @@ class Kaleido:
         open(os.path.join(meta_path, 'info', 'exclude'), 'at').write('.git' + '\n')
         open(os.path.join(meta_path, 'git-daemon-export-ok'), 'wt')
         open(os.path.join(meta_path, 'inbox-id'), 'wt').write(inbox_id + '\n')
-        self.gu.call(['repack', '-a', '-d'])    # to convert large pack files into small pack files bound to pack.packSizeLimit
+        #self.gu.call(['repack', '-a', '-d'])    # to convert large pack files into small pack files bound to pack.packSizeLimit
         self.gu.call(['checkout'])
         return True
 
@@ -721,6 +721,11 @@ class Kaleido:
                 continue
             self.gu.call(['add', '--force', path], False)
 
+            # commit local changes to local master
+            self.gu.call(['commit',
+                          '--author=%s <%s@%s>' % (getpass.getuser(), getpass.getuser(), platform.node()),
+                          '--message=', '--allow-empty-message'], False)
+
         # find removed files
         to_rm = self.gu.call(['ls-files', '--deleted', '-z'] + exclude_args + \
                              [self.options.working_copy_root], False)[1]
@@ -730,6 +735,11 @@ class Kaleido:
             if os.path.isdir(os.path.join(self.options.working_copy_root, path)):
                 continue
             self.gu.call(['rm', '--cached', path], False)
+
+            # commit local changes to local master
+            self.gu.call(['commit',
+                          '--author=%s <%s@%s>' % (getpass.getuser(), getpass.getuser(), platform.node()),
+                          '--message=', '--allow-empty-message'], False)
 
     def _sync(self, sync_forever):
         self.gu.detect_working_copy_root()
@@ -950,7 +960,12 @@ class Kaleido:
 
             # add .kaleido-git/index
             # once this is done, the working copy will be tracked even though it has .git symlink
-            self.gu.call(['add', fixed_git_path])
+            self.gu.call(['add', os.path.join(fixed_git_path, 'index')])
+
+            # commit local changes to local master
+            self.gu.call(['commit',
+                          '--author=%s <%s@%s>' % (getpass.getuser(), getpass.getuser(), platform.node()),
+                          '--message=', '--allow-empty-message'], False)
 
         return True
 
@@ -984,6 +999,9 @@ class Kaleido:
             # exclude all files of .kaleido-git as well as its working copy
             self.gu.call(['rm', '-r', '--cached', '--ignore-unmatch', root])
 
+            self.gu.call(['commit',
+                          '--author=%s <%s@%s>' % (getpass.getuser(), getpass.getuser(), platform.node()),
+                          '--message=', '--allow-empty-message'], False)
         return True
 
     def git_command(self, args):
